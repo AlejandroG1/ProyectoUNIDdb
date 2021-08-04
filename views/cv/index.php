8990bdc6f6ec4c155c0241227d52e3d76ad2420b
AlejandroG1 <jalegalarza@gmail.com>
2021-08-03 21:30:31 -0500

    Estructura proyecto
    Estructura basica del proyecto final

---
+++ views/cv/index.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <title>Curriculum</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <div class="container">
+        <div class="left_side">
+            <div class="text_perfil">
+                <div class="img_box">
+                    <img src="eren2.jpg">
+                </div>
+                <h2>Francisco Ivan <br><span>Estudiante de progamacion</span> </h2>
+            </div>
+
+            <div class="infoContact">
+                <h3 class="title">Informacion de contacto</h3>
+                <ul>
+                    <li>
+                        <span class="icon"><i class="fa fa-phone-square" aria-hidden="true"></i></span>
+                        <span class="text">+52 998 3142 008</span>
+                    </li>
+                    <li>
+                        <span class="icon"><i class="fa fa-envelope" aria-hidden="true"></i></span>
+                        <span class="text">Francisco@gmail.com</span>
+                    </li>
+                    <li>
+                        <span class="icon"><i class="fa fa-github" aria-hidden="true"></i></span>
+                        <span class="text">github.com/Francisco-splee</span>
+                    </li>
+                    <li>
+                        <span class="icon"><i class="fa fa-linkedin-square" aria-hidden="true"></i></span>
+                        <span class="text">linkedin.com/in/francisco-ivan-barrera-lorenzo-15a59a209</span>
+                    </li>
+                </ul>
+            </div>
+
+
+
+            <div class="infoContactEducation">
+                <h3 class="title">Educacion</h3>
+                <ul>
+                    <li>
+                    <h4>[Nombre de la escuela]</h4>
+                        <h5>2019 - presente</h5>
+                        <h4>[Puede presumir de sus buenas notas y mencionar las recompensas y honores recibidos. No dude en comentar brevemente sus tareas actuales]. </h4>
+                           
+                    </li>
+                    <li>
+                        <h4>[Nombre de la escuela]</h4>   
+                        <h5>2017 - 2019</h5>
+                        <h4>[Puede presumir de sus buenas notas y mencionar las recompensas y honores recibidos. No dude en comentar brevemente sus tareas actuales]. </h4>
+                        
+                    </li>
+                    <li>
+                        <h4>[Nombre de la escuela]</h4>
+                        <h5>2012 - 2016</h5>
+                        <h4>[Puede presumir de sus buenas notas y mencionar las recompensas y honores recibidos. No dude en comentar brevemente sus tareas actuales]. </h4>
+                           
+                    </li>
+                </ul>
+            </div>
+
+            <div class="infoContactLanguage">
+                <h3 class="title">Idiomas</h3>
+                <ul>
+                    <li>
+                        <span class="text">Español</span>
+                        <span class="percent">
+                            <div style="width: 85%;"></div>
+                        </span>
+                    </li>
+                    <li>
+                        <span class="text">Ingles</span>
+                        <span class="percent">
+                        <div style="width: 40%;"></div>
+                        </span>
+                    </li>
+                    <li>
+                        <span class="text">Frances</span>
+                        <span class="percent">
+                        <div style="width: 15%;"></div>
+                        </span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="right_side">
+            <div class="Acerca">
+                <h2 class="title2">Perfil</h2>
+                <p>Rutrum arcu libero sagittis eu penatibus montes commodo
+                    sociosqu, fermentum etiam massa placerat sem interdum enim maecenas, dictum fames metus
+                    vehicula inceptos scelerisque vestibulum. </p>
+            </div>
+            <div class="Acerca">
+                <h2 class="title2">Experiencia</h2>
+                <div class="box">
+                    <div class="yearCompany">
+                        <h5>2020 - presente</h5>
+                        <h5>[Nombre de la compañía]</h5>
+                    </div>
+                    <div class="text">
+                        <h4>[Puesto del trabajo]</h4>
+                        <p>[Describa sus responsabilidades y logros recalcando el impacto y los resultados conseguidos. Puede poner ejemplos, pero sea breve.] 
+</p>
+                    </div>
+                </div>
+                
+
+                <div class="box">
+                    <div class="yearCompany">
+                        <h5>2017 - 2019</h5>
+                        <h5>[Nombre de la compañía]</h5>
+                    </div>
+                    <div class="text">
+                        <h4>[Puesto del trabajo]</h4>
+                        <p>[Describa sus responsabilidades y logros recalcando el impacto y los resultados conseguidos. Puede poner ejemplos, pero sea breve.] 
+</p>
+                    </div>
+                </div>
+
+                <div class="box">
+                    <div class="yearCompany">
+                        <h5>2015 - 2017</h5>
+                        <h5>[Nombre de la compañía]</h5>
+                    </div>
+                    <div class="text">
+                        <h4>[Puesto del trabajo]</h4>
+                        [Describa sus responsabilidades y logros recalcando el impacto y los resultados conseguidos. Puede poner ejemplos, pero sea breve.] 
+</p>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="habilidades">
+                <h2 class="title2">Habilidades tecnicas</h2>
+            <div class="box">
+                <h4>Html</h4>
+                <div class="percent">
+                <div style="width: 85%;"></div>
+                </div>
+            </div>
+            <div class="box">
+                <h4>Css</h4>
+                <div class="percent">
+                <div style="width: 75%;"></div>
+                </div>
+            </div>
+            <div class="box">
+                <h4>C++</h4>
+                <div class="percent">
+                <div style="width: 60%;"></div>
+                </div>
+            </div>
+            <div class="box">
+                <h4>JavaScript</h4>
+                <div class="percent">
+                <div style="width: 40%;"></div>
+                </div>
+            </div>
+            <div class="box">
+                <h4>Php</h4>
+                <div class="percent">
+                <div style="width: 30%;"></div>
+                </div>
+            </div>
+            <div class="box">
+                <h4>Photoshop</h4>
+                <div class="percent">
+                <div style="width: 50%;"></div>
+                </div>
+            </div>
+            <div class="box">
+                <h4>Office</h4>
+                <div class="percent">
+                <div style="width: 90%;"></div>
+                </div>
+            </div>
+            </div>
+
+            <div class="pasatiempos">
+                <h2 class="title2">Pasatiempos</h2>
+                <ul>
+                    <li><i class="fa fa-gamepad" aria-hidden="true"></i>Videojuegos</li>
+                    <li><i class="fa fa-music" aria-hidden="true"></i>Musica</li>
+                    <li><i class="fa fa-book" aria-hidden="true"></i>lectura</li>
+                    <li><i class="fa fa-cutlery" aria-hidden="true"></i>cocinar</li>
+                    <li><i class="fa fa-paint-brush" aria-hidden="true"></i>dibujar</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ views/cv2/index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" media="all">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <title>Curriculum</title>
+</head>
+<body>
+    <header class="cabezera row">
+            <div class="foto col-2">
+                <p>aqui va tu foto</p>
+            </div>
+            <div class="col-5 name">
+                <h3>Nombre_1 Nombre_2</h3>
+                <h3>Apellido_P Apellido_M</h3>
+                <p>Especialidad</p>
+            </div>
+            <div class="col-4 contact">
+                <h3 class="contacto" >CONTACTO</h3>
+                <p>correo:</p>
+                <p>Telefono:</p>
+                <p>Direccion:</p>   
+                <p></p>
+            </div>
+    </header>
+    <div class="acerca_de">
+        <br>
+        <h3>ACERCA DE MI</h3> 
+        <br>
+        <p class="texto-user">
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia vo
+
+
+        </p>
+    </div>
+    <div class="experiencia">
+        <br>
+        <h3>EXPERIENCIA LABORAL</h3>
+        <br>
+          <div class="row">
+            <p class="col-4">Lugar de trabajo:</p>  
+            <p class="col-4">Descripcion de labores:</p>
+            <p class="col-4">Tiempo trabajado:</p>
+          </div>
+          <div class="row">
+              <p class="col-4 texto-user">experiencia 1</p>
+              <p class="col-4 texto-user" >experiencia 1</p>
+              <p class="col-4 texto-user">experiencia 1</p>
+          </div>
+         
+          <div class="row">
+              <p class="col-4 texto-user">experiencia 2</p>
+              <p class="col-4 texto-user" >experiencia 2</p>
+              <p class="col-4 texto-user">experiencia 2</p>
+          </div>
+    </div>
+    <div class="academica">
+        <br>
+        <h3>INFORMACION ACADEMICA</h3>
+        <br>
+        <div class="row">
+            <p class="col-4">Lugar de estudio:</p>
+            <p class="col-4">Descripcion de estudios:</p>
+            <p class="col-4">Tiempo de cursamiento:</p>
+        </div>
+        <div class="row">
+              <p class="col-4 texto-user">estudios 1</p>
+              <p class="col-4 texto-user" >estudios 1</p>
+              <p class="col-4 texto-user">estudios 1</p>
+          </div>
+          <div class="row">
+              <p class="col-4 texto-user">estudios 2</p>
+              <p class="col-4 texto-user" >estudios 2</p>
+              <p class="col-4 texto-user">estudios 2</p>
+          </div>
+    </div>
+    <div class="habilidades">
+        <br>
+        <h3>HABILIDADES TECNICAS</h3>
+        <br>
+        <div class="row">
+            <p class="col-4">Habilidad:</p>
+            <p class="col-4">Nivel de dominio considerado:</p>
+            <p class="col-4">Descripcion de habilidad:</p>
+        </div>
+        <div class="row">
+              <p class="col-4 texto-user">Habilidad 1</p>
+              <p class="col-4 texto-user">Habilidad 1</p>
+              <p class="col-4 texto-user">Habilidad 1</p>
+          </div>
+          <div class="row">
+              <p class="col-4 texto-user">Habilidad 2</p>
+              <p class="col-4 texto-user">Habilidad 2</p>
+              <p class="col-4 texto-user">Habilidad 2</p>
+          </div>
+    </div>
+    <div class="idiomas">
+        <h3>IDIOMAS</h3>
+        <div class="row">
+            <p class="col-4">Idioma:</p>
+            <p class="col-4">Porcentaje considerado:</p>
+            <p class="col-4">Link de mi certificado:</p>
+        </div>
+        <div class="row">
+              <p class="col-4 texto-user">idioma 1</p>
+              <p class="col-4 texto-user">idioma 1</p>
+              <p class="col-4 texto-user">idioma 1</p>
+          </div>
+          <div class="row">
+              <p class="col-4 texto-user">idioma 2</p>
+              <p class="col-4 texto-user">idioma 2</p>
+              <p class="col-4 texto-user">idioma 2</p>
+          </div>
+    </div>
+    <div class="row extras">
+        <div class="col-6 pasatiempos">
+            <br>
+            <h3>COMPETENCIAS</h3>
+            <br>
+                <p>pasatiempo 1</p>
+                <p>pasatiempo 2</p>
+                <p>pasatiempo 3</p>
+                <p>pasatiempo 4</p>
+        </div>
+        <div class="col-6 competencias">
+            <br>
+            <h3>COMPETENCIAS</h3>
+            <br>
+            <p>competencia 1</p>
+            <p>competencia 2</p>
+            <p>competencia 3</p>
+            <p>competencia 4</p>
+
+        </div>
+
+    </div>
+</body>
+</html>
