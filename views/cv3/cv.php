9dac84dc388b895122fa03fd8a3a0afd7b60919d
Emmanuel <55505148+Emmanuel-XD@users.noreply.github.com>
2021-08-05 18:49:29 -0500

    Add files via upload
    Se actualizo el diseño del cv

---
+++ views/cv3/cv.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <title>Curriculum</title>
+</head>
+<body>
+
+<div class="contenedor">
+    <div class="principal" >
+        <div class="header" >
+            <div>
+                <img src="imges.jpg" width="80%">
+            </div>
+
+            <div class="nombre">
+            <h1>Emmanuel Poot Mugarte</h1>
+            <p><b>Direccion:</b> SM251 M79 AV.HERIBERTO CASTILLO, Paseos del mar.
+            <br>
+            <b>Celular:</b> 9981571220 - <b>Correo:</b> Mugarte5672@gmail.com
+             </p>
+</div>
+</div>
+</div>
+
+<div class="cuerpo">   
+<div class="contenido">
+    <h2 class="titulo">Objetivos</h2>
+    <p>
+        Pasar la meteria del profe abraham 
+    </p>
+</div>
+
+<div class="contenido">
+    <h2 class="titulo">Formacion Academica</h2>
+    <p><b>UNID</b><br/>
+    Estudiante de ingenieria en software y sistemas computacionales
+    <br/>
+    <br/>
+    <b>Escuela 2</b>
+</br>
+    Titulo obtenido: 
+
+    </p>
+</div>
+
+<div class="contenido">
+    <h2 class="titulo">Certificados</h2>
+    <p>-<b>Certificado</b>: JavaScript<br/>
+    -<b>Certificado</b>: Java<br/>
+    -<b>Certificado</b>: Php<br/>
+    -<b>Certificado</b>: C++<br/>
+
+    </p>
+</div>
+<div class="contenido">
+    <h2 class="titulo">Experiencia laboral</h2>
+    *<b>Programador</b>,
+     Puesto: Progrmador Junior ------------------- (Mayo 2021 - Octubre 2021) <br></br>
+     *<b>Axuliar de sistemas</b>,
+     Puesto: Soporte tecnico ------------------- (Mayo 2021 - Octubre 2021) <br></br>
+    </p>
+</div>
+<div class="contenido">
+    <h2 class="titulo">Competencias</h2>
+    <p>-<b> Tolerancia a la frustracion <br/>
+    <p>-<b> Trabajo en equipo<br/>
+    <p>-<b> Toma de decisiones<br/>
+    <p>-<b> Adaptacion a cambios<br/>
+  
+    </p>
+</div>
+
+</div>
+</body>
+</html>
